e11bc471b4bbeab765ec18ffa8d57abe23266370
RichardAri <richardaripari@gmail.com>
2022-06-28 22:20:22 -0500

    Update reportes

--- src/Maletas/GenerarReportes.java
+++ src/Maletas/GenerarReportes.java
@@ -92,7 +92,7 @@ public class GenerarReportes extends JFrame implements ItemListener {
 			itemStateChangedCmbTipoReporte(e);
 
 	}
-	
+
 	public void itemStateChangedCmbTipoReporte(ItemEvent e) {
 		int index = this.cmbTipoReporte.getSelectedIndex();
 		switch (index) {
@@ -113,131 +113,191 @@ public class GenerarReportes extends JFrame implements ItemListener {
 		}
 
 	}
-	
-	
+
 	private void reportePorRelacionPrecio() {
 		textAreaReporte.setText("RAZON DE PRECIOS EN RELACION AL PRECIO MAYOR\n");
 		textAreaReporte.append("Modelo : Aviator\n");
-		textAreaReporte.append("Precio : "+VentanaMaletas.cantidadVentasAviator+"\n");
-		textAreaReporte.append("Razon : "+VentanaMaletas.cantidadUnidadesVendidasAviator+"\n");
-		
+		textAreaReporte.append("Precio : " + VentanaMaletas.cantidadVentasAviator + "\n");
+		textAreaReporte.append("Razon : " + VentanaMaletas.cantidadUnidadesVendidasAviator + "\n");
+
 		textAreaReporte.append("Modelo : Century\n");
-		textAreaReporte.append("Precio : "+VentanaMaletas.cantidadVentasCentury+"\n");
-		textAreaReporte.append("Razon : "+VentanaMaletas.cantidadUnidadesVendidasCentury+"\n");
+		textAreaReporte.append("Precio : " + VentanaMaletas.cantidadVentasCentury + "\n");
+		textAreaReporte.append("Razon : " + VentanaMaletas.cantidadUnidadesVendidasCentury + "\n");
 
 		textAreaReporte.append("Modelo : Benneton\n");
-		textAreaReporte.append("Precio : "+VentanaMaletas.cantidadVentasBenneton+"\n");
-		textAreaReporte.append("Razon : "+VentanaMaletas.cantidadUnidadesVendidasBenneton+"\n");
+		textAreaReporte.append("Precio : " + VentanaMaletas.cantidadVentasBenneton + "\n");
+		textAreaReporte.append("Razon : " + VentanaMaletas.cantidadUnidadesVendidasBenneton + "\n");
 
 		textAreaReporte.append("Modelo : Lucas\n");
-		textAreaReporte.append("Precio : "+VentanaMaletas.cantidadVentasLucas+"\n");
-		textAreaReporte.append("Razon : "+VentanaMaletas.cantidadUnidadesVendidasLucas+"\n");
+		textAreaReporte.append("Precio : " + VentanaMaletas.cantidadVentasLucas + "\n");
+		textAreaReporte.append("Razon : " + VentanaMaletas.cantidadUnidadesVendidasLucas + "\n");
 
 		textAreaReporte.append("Modelo : Samsonite\n");
-		textAreaReporte.append("Precio : "+VentanaMaletas.cantidadVentasSamsonite+"\n");
-		textAreaReporte.append("Razon : "+VentanaMaletas.cantidadUnidadesVendidasSamsonite);
-		
-	}
+		textAreaReporte.append("Precio : " + VentanaMaletas.cantidadVentasSamsonite + "\n");
+		textAreaReporte.append("Razon : " + VentanaMaletas.cantidadUnidadesVendidasSamsonite);
 
-	
+	}
 
 	private void reportePorCantidadOptima() {
 		int superaron = 0;
 		int noSuperaron = 0;
 		int igualaron = 0;
-		if(VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadOptima)
-			superaron +=1;
-		else if(VentanaMaletas.cantidadUnidadesVendidasAviator == VentanaMaletas.cantidadOptima)
-			igualaron+=1;
+		if (VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadOptima)
+			superaron += 1;
+		else if (VentanaMaletas.cantidadUnidadesVendidasAviator == VentanaMaletas.cantidadOptima)
+			igualaron += 1;
 		else {
-			noSuperaron +=1;
+			noSuperaron += 1;
 		}
-		
-		if(VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadOptima)
-			superaron +=1;
-		else if(VentanaMaletas.cantidadUnidadesVendidasCentury == VentanaMaletas.cantidadOptima)
-			igualaron+=1;
+
+		if (VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadOptima)
+			superaron += 1;
+		else if (VentanaMaletas.cantidadUnidadesVendidasCentury == VentanaMaletas.cantidadOptima)
+			igualaron += 1;
 		else {
-			noSuperaron +=1;
+			noSuperaron += 1;
 		}
-		
-		if(VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadOptima)
-			superaron +=1;
-		else if(VentanaMaletas.cantidadUnidadesVendidasBenneton == VentanaMaletas.cantidadOptima)
-			igualaron+=1;
+
+		if (VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadOptima)
+			superaron += 1;
+		else if (VentanaMaletas.cantidadUnidadesVendidasBenneton == VentanaMaletas.cantidadOptima)
+			igualaron += 1;
 		else {
-			noSuperaron +=1;
+			noSuperaron += 1;
 		}
-		
-		if(VentanaMaletas.cantidadUnidadesVendidasLucas > VentanaMaletas.cantidadOptima)
-			superaron +=1;
-		else if(VentanaMaletas.cantidadUnidadesVendidasLucas == VentanaMaletas.cantidadOptima)
-			igualaron+=1;
+
+		if (VentanaMaletas.cantidadUnidadesVendidasLucas > VentanaMaletas.cantidadOptima)
+			superaron += 1;
+		else if (VentanaMaletas.cantidadUnidadesVendidasLucas == VentanaMaletas.cantidadOptima)
+			igualaron += 1;
 		else {
-			noSuperaron +=1;
+			noSuperaron += 1;
 		}
-		
-		if(VentanaMaletas.cantidadUnidadesVendidasSamsonite > VentanaMaletas.cantidadOptima)
-			superaron +=1;
-		else if(VentanaMaletas.cantidadUnidadesVendidasSamsonite == VentanaMaletas.cantidadOptima)
-			igualaron+=1;
+
+		if (VentanaMaletas.cantidadUnidadesVendidasSamsonite > VentanaMaletas.cantidadOptima)
+			superaron += 1;
+		else if (VentanaMaletas.cantidadUnidadesVendidasSamsonite == VentanaMaletas.cantidadOptima)
+			igualaron += 1;
 		else {
-			noSuperaron +=1;
+			noSuperaron += 1;
 		}
 
 		textAreaReporte.setText("ESTADISTICA SOBRE LA CANTIDA OPTIMA\n");
-		textAreaReporte.append("Cantidad de modelos que superaron la cantidad optima : "+superaron+"\n");
-		textAreaReporte.append("Cantidad de modelos que no superaron la cantidad optima : "+noSuperaron+"\n");
-		textAreaReporte.append("Cantidad de modelos que igualaron la cantidad optima : "+igualaron);
-		
+		textAreaReporte.append("Cantidad de modelos que superaron la cantidad optima : " + superaron + "\n");
+		textAreaReporte.append("Cantidad de modelos que no superaron la cantidad optima : " + noSuperaron + "\n");
+		textAreaReporte.append("Cantidad de modelos que igualaron la cantidad optima : " + igualaron);
+
 	}
-	
+
 	private void reportePorPrecio() {
-		//Declaracion de variables
+		// Declaracion de variables
 		double promedio = 0;
 		double mayor = 0;
-		double menor = 0 ;
-		
-		if(VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadUnidadesVendidasCentury && 
-				VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadUnidadesVendidasBenneton &&
-				VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadUnidadesVendidasLucas && 
-				VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadUnidadesVendidasSamsonite )
+		double menor = 0;
+		// if Aviator = Mayor : else menor
+		if (VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadUnidadesVendidasCentury
+				&& VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadUnidadesVendidasBenneton
+				&& VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadUnidadesVendidasLucas
+				&& VentanaMaletas.cantidadUnidadesVendidasAviator > VentanaMaletas.cantidadUnidadesVendidasSamsonite)
 			mayor = VentanaMaletas.cantidadUnidadesVendidasAviator;
+
+		if (VentanaMaletas.cantidadUnidadesVendidasAviator < VentanaMaletas.cantidadUnidadesVendidasCentury
+				&& VentanaMaletas.cantidadUnidadesVendidasAviator < VentanaMaletas.cantidadUnidadesVendidasBenneton
+				&& VentanaMaletas.cantidadUnidadesVendidasAviator < VentanaMaletas.cantidadUnidadesVendidasLucas
+				&& VentanaMaletas.cantidadUnidadesVendidasAviator < VentanaMaletas.cantidadUnidadesVendidasSamsonite)
+			menor = VentanaMaletas.cantidadUnidadesVendidasAviator;
+
+		// Century Mayor
+		if (VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadUnidadesVendidasAviator
+				&& VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadUnidadesVendidasBenneton
+				&& VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadUnidadesVendidasLucas
+				&& VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadUnidadesVendidasSamsonite)
+			mayor = VentanaMaletas.cantidadUnidadesVendidasCentury;
+
+		if (VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadUnidadesVendidasAviator
+				&& VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadUnidadesVendidasBenneton
+				&& VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadUnidadesVendidasLucas
+				&& VentanaMaletas.cantidadUnidadesVendidasCentury > VentanaMaletas.cantidadUnidadesVendidasSamsonite)
+			menor = VentanaMaletas.cantidadUnidadesVendidasCentury;
+		// Benneton Mayor
+		if (VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadUnidadesVendidasAviator
+				&& VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadUnidadesVendidasCentury
+				&& VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadUnidadesVendidasLucas
+				&& VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadUnidadesVendidasSamsonite)
+			mayor = VentanaMaletas.cantidadUnidadesVendidasBenneton;
+		if (VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadUnidadesVendidasAviator
+				&& VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadUnidadesVendidasCentury
+				&& VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadUnidadesVendidasLucas
+				&& VentanaMaletas.cantidadUnidadesVendidasBenneton > VentanaMaletas.cantidadUnidadesVendidasSamsonite)
+			menor = VentanaMaletas.cantidadUnidadesVendidasBenneton;
+		// Lucas Mayor
+		if (VentanaMaletas.cantidadUnidadesVendidasLucas > VentanaMaletas.cantidadUnidadesVendidasAviator
+				&& VentanaMaletas.cantidadUnidadesVendidasLucas > VentanaMaletas.cantidadUnidadesVendidasBenneton
+				&& VentanaMaletas.cantidadUnidadesVendidasLucas > VentanaMaletas.cantidadUnidadesVendidasCentury
+				&& VentanaMaletas.cantidadUnidadesVendidasLucas > VentanaMaletas.cantidadUnidadesVendidasSamsonite)
+			mayor = VentanaMaletas.cantidadUnidadesVendidasLucas;
+		if (VentanaMaletas.cantidadUnidadesVendidasLucas < VentanaMaletas.cantidadUnidadesVendidasAviator
+				&& VentanaMaletas.cantidadUnidadesVendidasLucas < VentanaMaletas.cantidadUnidadesVendidasBenneton
+				&& VentanaMaletas.cantidadUnidadesVendidasLucas < VentanaMaletas.cantidadUnidadesVendidasCentury
+				&& VentanaMaletas.cantidadUnidadesVendidasLucas < VentanaMaletas.cantidadUnidadesVendidasSamsonite)
+			menor = VentanaMaletas.cantidadUnidadesVendidasLucas;
+		// Samsonite Mayor
+		if (VentanaMaletas.cantidadUnidadesVendidasSamsonite > VentanaMaletas.cantidadUnidadesVendidasAviator
+				&& VentanaMaletas.cantidadUnidadesVendidasSamsonite > VentanaMaletas.cantidadUnidadesVendidasBenneton
+				&& VentanaMaletas.cantidadUnidadesVendidasSamsonite > VentanaMaletas.cantidadUnidadesVendidasLucas
+				&& VentanaMaletas.cantidadUnidadesVendidasSamsonite > VentanaMaletas.cantidadUnidadesVendidasCentury)
+			mayor = VentanaMaletas.cantidadUnidadesVendidasSamsonite;
+		if (VentanaMaletas.cantidadUnidadesVendidasSamsonite < VentanaMaletas.cantidadUnidadesVendidasAviator
+				&& VentanaMaletas.cantidadUnidadesVendidasSamsonite < VentanaMaletas.cantidadUnidadesVendidasBenneton
+				&& VentanaMaletas.cantidadUnidadesVendidasSamsonite < VentanaMaletas.cantidadUnidadesVendidasLucas
+				&& VentanaMaletas.cantidadUnidadesVendidasSamsonite < VentanaMaletas.cantidadUnidadesVendidasCentury)
+			menor = VentanaMaletas.cantidadUnidadesVendidasSamsonite;
 		
-		
+		promedio = (VentanaMaletas.cantidadUnidadesVendidasAviator+
+				VentanaMaletas.cantidadUnidadesVendidasCentury+
+				VentanaMaletas.cantidadUnidadesVendidasBenneton+
+				VentanaMaletas.cantidadUnidadesVendidasLucas+
+				VentanaMaletas.cantidadUnidadesVendidasSamsonite)/5;
+
 		textAreaReporte.setText("ESTADISTICA SOBRE EL PRECIO\n");
-		
-		
+		textAreaReporte.append("Precio promedio : S/."+promedio+"\n");
+		textAreaReporte.append("Precio mayor : S/."+mayor+"\n");
+		textAreaReporte.append("Precio menor : S/."+menor);
+
 	}
 
 	public void reportePorModelos() {
 		textAreaReporte.setText("VENTAS POR MODELO\n");
 		textAreaReporte.append("Modelo				 		  : Aviator\n");
-		textAreaReporte.append("Cantidad de Ventas  	      : "+VentanaMaletas.cantidadVentasAviator+"\n");
-		textAreaReporte.append("Cantidad de Unidades Vendidas : "+VentanaMaletas.cantidadUnidadesVendidasAviator+"\n");
-		textAreaReporte.append("Import total vendido 		  : "+VentanaMaletas.impTotalAviator+"\n");
-		
+		textAreaReporte.append("Cantidad de Ventas  	      : " + VentanaMaletas.cantidadVentasAviator + "\n");
+		textAreaReporte
+				.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasAviator + "\n");
+		textAreaReporte.append("Import total vendido 		  : " + VentanaMaletas.impTotalAviator + "\n");
+
 		textAreaReporte.append("Modelo				 		  : Century\n");
 		textAreaReporte.append("Cantidad de Ventas  	      : " + VentanaMaletas.cantidadVentasCentury + "\n");
-		textAreaReporte.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasCentury + "\n");
+		textAreaReporte
+				.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasCentury + "\n");
 		textAreaReporte.append("Import total vendido 		  : " + VentanaMaletas.impTotalCentury + "\n");
-		
+
 		textAreaReporte.append("Modelo				 		  : Benneton\n");
 		textAreaReporte.append("Cantidad de Ventas  	      : " + VentanaMaletas.cantidadVentasBenneton + "\n");
-		textAreaReporte.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasBenneton + "\n");
+		textAreaReporte
+				.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasBenneton + "\n");
 		textAreaReporte.append("Import total vendido 		  : " + VentanaMaletas.impTotalBenneton + "\n");
-		
+
 		textAreaReporte.append("Modelo				 		  : Lucas\n");
 		textAreaReporte.append("Cantidad de Ventas  	      : " + VentanaMaletas.cantidadVentasLucas + "\n");
-		textAreaReporte.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasLucas + "\n");
+		textAreaReporte
+				.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasLucas + "\n");
 		textAreaReporte.append("Import total vendido 		  : " + VentanaMaletas.impTotalLucas + "\n");
-		
+
 		textAreaReporte.append("Modelo				 		  : Samsonite\n");
 		textAreaReporte.append("Cantidad de Ventas  	      : " + VentanaMaletas.cantidadVentasSamsonite + "\n");
-		textAreaReporte.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasSamsonite + "\n");
+		textAreaReporte
+				.append("Cantidad de Unidades Vendidas : " + VentanaMaletas.cantidadUnidadesVendidasSamsonite + "\n");
 		textAreaReporte.append("Import total vendido 		  : " + VentanaMaletas.impTotalSamsonite);
-		
-	}
 
+	}
 
 }
